0a9fbc1f4b690c19a85228c71e931d1c9da42c37
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2021-10-22 09:28:46 +0600

    complete

---
+++ src/components/UpdateUser/UpdateUser.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+const UpdateUser = () => {
+    const [user, setUser] = useState([]);
+    const { id } = useParams();
+    useEffect(() => {
+        const url = `http://localhost:5000/users/${id}`;
+        fetch(url)
+            .then(res => res.json())
+        .then(data => setUser(data))
+    },[])
+    return (
+        <div>
+            <h2>Update User: {user.name}</h2>
+        </div>
+    );
+};
+
+export default UpdateUser;
